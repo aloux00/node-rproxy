56ee3145def14e4e7d06dae943da64639bf5be22
nickolanack <nickblackwell82@gmail.com>
2015-07-30 20:51:38 -0700

    link source and destination with websockets

--- server.js
+++ server.js
@@ -5,12 +5,51 @@
 
 var WebSocket = require('ws');
 if(process.argv[2].indexOf('ws:')!==0){
-	throw new Error('Requires websocket address argument: ie: www.host.com:port/path');
+	throw new Error('Requires websocket source address argument: ie: www.host.com:port/path');
 }
-var ws = new WebSocket(process.argv[2]);
 
-ws.on('open', function open() {
-	 
-		console.log('opened');
+if(process.argv[3].indexOf('ws:')!==0){
+	throw new Error('Requires websocket destination address argument: ie: www.host.com:port/path');
+}
+
+
+
+
+var run=function(){
+	(new WebSocket(process.argv[2])).once('message', function message(data, flags) {
+		
+		var a=this;
+		
+		(new WebSocket(process.argv[3])).on('open', function open() {
+			
+			var b=this;
+			b.send(data);
+			a.on('message', function message(data, flags) {
+				b.send(data);
+			}).on('error',function(error){
+				console.log('a error: '+error)
+			}).on('close',function(code, message){
+				console.log('a close: '+code+' '+message);
+				a=null;
+				if(b){
+					b.close();
+				}
+			});
+			b.on('message', function message(data, flags) {
+				a.send(data);
+			}).on('error',function(error){
+				console.log('b error: '+error)
+			}).on('close',function(code, message){
+				console.log('b close: '+code+' '+message);
+				b=null;
+				if(a){
+					a.close();
+				}
+				
+			});
+		});
 	
 	});
+};
+
+run();
