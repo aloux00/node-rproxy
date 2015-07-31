206ddc96acad3d12a58c3e74f6e41e72a426c40c
nickolanack <nickblackwell82@gmail.com>
2015-07-30 20:12:58 -0700

    1->2

--- server.js
+++ server.js
@@ -4,10 +4,10 @@
 
 
 var WebSocket = require('ws');
-if(process.argv[1].indexOf('ws:')!==0){
+if(process.argv[2].indexOf('ws:')!==0){
 	throw new Error('Requires websocket address argument: ie: www.host.com:port/path');
 }
-var ws = new WebSocket(process.argv[1]);
+var ws = new WebSocket(process.argv[2]);
 
 ws.on('open', function open() {
 	 
