02cade03fa94bbf889123b9830ef4fcac53f25cc
nickolanack <nickblackwell82@gmail.com>
2015-07-30 21:47:13 -0700

    log

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ if(process.argv[3].indexOf('ws:')!==0){
 
 
 var run=function(){
-	(new WebSocket(process.argv[2])).on('connect').once('message', function message(data, flags) {
+	(new WebSocket(process.argv[2])).on('open',function(){
+		
+		console.log('connected proxy');
+		
+	}).once('message', function message(data, flags) {
 		
 		var a=this;
 		
