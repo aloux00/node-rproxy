4275d01c1a0adc58760f986282e541146ba83a09
nickolanack <nickblackwell82@gmail.com>
2015-07-30 18:45:23 -0700

    added main files, creates a websocket and webserver pair

---
+++ index.js
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+
+var config=require('./server.json');
+
+
+(function(){
+	
+	//Simple webserver
+
+	var fs=require('fs');
+	var http=require('http');
+	var async=require('async');
+	var port=config.serverPort;
+	var documentRoot='./html/';
+
+	var server=http.createServer(function(req, res) {
+
+		
+	});
+	
+	server.listen(port);
+	console.log('webserver listening on: '+port);
+
+})();
+
+
+(function(){
+
+	// Simple websocket server
+	
+	var port = config.websocketPort;
+	var clients = [];
+
+	(new (require('ws').Server)({
+		port: port
+	})).on('connection', function(wsclient){
+	
+		clients.push(wsclient);
+		console.log('client connected: '+wsclient);
+
+		wsclient.on('message',function(data){
+
+		}).on('close',function(code, message){
+
+		});
+
+	}).on('error', function(error){
+		
+		console.log('error: '+error);
+	
+	});
+		
+	console.log('websocket listening on: '+port);
+
+})();
+
+
+
+
