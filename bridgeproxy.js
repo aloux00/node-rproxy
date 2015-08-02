e2add792d186c7bf0830df6ae1e3f58f62ce7a31
nickolanack <nickblackwell82@gmail.com>
2015-08-01 21:57:26 -0700

    btoa issue?

--- bridgeproxy.js
+++ bridgeproxy.js
@@ -21,11 +21,12 @@ function WSBridgeProxy(config){
 
 		if((typeof wsclient.upgradeReq.headers.authorization)!='undefined'){
 			console.log(wsclient.upgradeReq)
-			var basicauth=btoa(wsclient.upgradeReq.headers.authorization.split(' ')[1]);
+			var b64auth=wsclient.upgradeReq.headers.authorization.split(' ')[1];
+			var basicauth=btoa(b64auth);
 			if(basicauth===config.basicauth){
 				serverconnections.push(wsclient)
 			}else{
-				console.log('Basic auth attempt invalid: '+basicauth+' | '+config.basicauth)
+				console.log('Basic auth attempt invalid: '+b64auth+' = ' +basicauth+' | '+config.basicauth)
 				wsclient.close(3000,'Basic auth attempt invalid');
 			}
 		}else{
