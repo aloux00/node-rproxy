cad14208562a3fa5c877b7ba1b5feb042c136276
nickolanack <nickblackwell82@gmail.com>
2015-08-03 18:00:10 -0700

    added simple socket buffering to clients added event emiter implementation to bridgeproxy

--- bridgeproxy.js
+++ bridgeproxy.js
@@ -4,75 +4,46 @@
  */
 
 
-
+var events = require('events');
 function WSBridgeProxy(config){
-
+	
 	// Simple websocket server
 	var me=this;
+	events.EventEmitter.call(me);
 	var port = config.port;
-	var serverconnections=[];
-	var clientconnections=[];
+	var freeServerConnections=[];
+	var freeClientConnections=[];
 
-	var atob=require('atob');
-
-
-
+	
 	me._server=(new (require('ws').Server)({
 		port: port
 	})).on('connection', function(wsclient){
 
-		if((typeof wsclient.upgradeReq.headers.authorization)!='undefined'){
-			//console.log(wsclient.upgradeReq)
-			var b64auth=wsclient.upgradeReq.headers.authorization.split(' ')[1];
-			var basicauth=atob(b64auth);
-			if(basicauth===config.basicauth){
-				serverconnections.push(wsclient)
+		if(me._isSocketAttemptingAuth(wsclient)){
+		
+			if(me._authorizeSocketAsServerConnection(wsclient, config.basicauth)){
+				freeServerConnections.push(wsclient)
 				console.log('bridge recieved server socket');
 			}else{
-				console.log('bridge basic auth attempt invalid: '+b64auth+' = ' +basicauth+' | '+config.basicauth)
 				wsclient.close(3000,'bridge basic auth attempt invalid');
+				return;
 			}
 		}else{
-			clientconnections.push(wsclient);
+			freeClientConnections.push(wsclient);
 			console.log('bridge recieved client socket');
+			me._bufferSocket(wsclient);
+			clientCounter++;
 		}
 
-		while(serverconnections.length&&clientconnections.length){
+		
+		
+		
 
-			console.log('bridge paired sockets: server::client');
+		while(freeServerConnections.length&&freeClientConnections.length){
 
-			var server=serverconnections.shift();
-			var client=clientconnections.shift();
-
-			server.on('message', function message(data, flags) {
-				console.log('bridge server sends: '+(typeof data));
-				client.send(data);
-			}).on('error',function(error){
-				console.log('bridge server error: '+error)
-			}).on('close',function(code, message){
-				console.log('bridge server close: '+code+' '+message);
-				server=null;
-				if(client){
-					client.close();
-				}
-			});
-
-			client.on('message', function message(data, flags) {
-				console.log('bridge client sends: '+(typeof data));
-				server.send(data);
-
-			}).on('error',function(error){
-				console.log('bridge client error: '+error)
-			}).on('close',function(code, message){
-				console.log('bridge client  close: '+code+' '+message);
-				client=null;
-				if(server){
-					server.close();
-				}
-
-			});
-
-
+			var server=freeServerConnections.shift();
+			var client=freeClientConnections.shift();
+			me._connectSockets(server, client)
 
 		}
 
@@ -82,11 +53,105 @@ function WSBridgeProxy(config){
 
 	}).on('error', function(error){
 		console.log('error: '+error);
-	})
+	});
 
 	console.log('websocket listening on: '+port);
 
 };
+WSBridgeProxy.prototype.__proto__ = events.EventEmitter.prototype;
+WSBridgeProxy.prototype._bufferSocket=function(wsclient){
+	var me=this;
+	if(!me._flushBuffers){
+		
+		me._bufferedClients=[];
+		me._buffers=[];
+		me._handlers=[];
+		
+		me._flushBuffers=function(server, client){
+			var i=me._bufferedClients.indexOf(client);
+			me._buffers[i].foreEach(function(message){
+				server.send(message);
+			});
+			
+			me._bufferedClients.splice(i,1);
+			me._buffers.splice(i,1);
+			client.removeEventListener('message', me._handlers[i]);
+			me._handlers.splice(i,1);
+		}
+		
+		me.on('pair', me._flushBuffers);
+	}
+	
+	
+	me._bufferedClients.push(wsclient);
+	var buffer=[];
+	me._buffers.push([]);
+	var handler=function message(data, flags) {
+		buffer.push(data);
+	}
+	me._handlers.push(handler);
+	wsclient.on('message', handler);
+	
+};
+
+WSBridgeProxy.prototype._isSocketAttemptingAuth=function(wsclient){
+	return (typeof wsclient.upgradeReq.headers.authorization)!='undefined'
+};
+
+WSBridgeProxy.prototype._authorizeSocketAsServerConnection=function(wsclient, basicauth){
+	var atob=require('atob');
+	var b64auth=wsclient.upgradeReq.headers.authorization.split(' ')[1];
+	var auth=atob(b64auth);
+	if(auth===basicauth){
+		return true;
+	}else{
+		console.log('bridge basic auth attempt invalid: '+b64auth+' = ' +auth+' | '+basicauth)
+		return false;
+	}
+};
+
+WSBridgeProxy.prototype._connectSockets=function(server, client){
+	console.log('bridge paired sockets: server::client');
+
+	var me=this;
+
+	server.on('message', function message(data, flags) {
+		console.log('bridge server sends: '+(typeof data));
+		client.send(data);
+	}).on('error',function(error){
+		console.log('bridge server error: '+error)
+	}).on('close',function(code, message){
+		console.log('bridge server close: '+code+' '+message);
+		server=null;
+		if(client){
+			me.emit('unpair', [server, client]);
+			client.close();
+		}
+	});
+
+	client.on('message', function message(data, flags) {
+		console.log('bridge client sends: '+(typeof data));
+		server.send(data);
+
+	}).on('error',function(error){
+		console.log('bridge client error: '+error)
+	}).on('close',function(code, message){
+		console.log('bridge client  close: '+code+' '+message);
+		client=null;
+		if(server){
+			me.emit('unpair', [server, client]);
+			server.close();
+		}
+		
+		
+
+	});
+	
+	me.emit('pair', [server, client]);
+	
+	
+	
+}
 
 WSBridgeProxy.prototype.close=function(){
 
